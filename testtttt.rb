ad8d0604fedec47fcd015ee7614d2927c0f44c18
Sidneyyy <catitude14@outlook.com>
2018-07-11 18:10:34 +0000

    automatically backed up by learn

--- testtttt.rb
+++ testtttt.rb
@@ -3,7 +3,15 @@ new_numbers = []
 numbers = [1, 2, 3, 4]
 numbers.each do |x|
   new_numbers = x + 1
-  new_numbers << new_numbers
+  new_numbers << new_number
 end
 
 puts new_numbers
+
+loop do
+numbers = [1, 2, 3, 4]
+numbers.each do |x|
+  puts x
+  break
+end
+end
